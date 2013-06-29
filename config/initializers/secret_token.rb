b598e22a66fdd6d760a981cd8220dff2f4b7388a
John Figueiredo <john@johnfigueiredo.com>
2013-06-28 22:08:16 -0700

    let the games begin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TeachersPet::Application.config.secret_token = 'bd56e9d7f095eedba83921f469ffd3b37a5554f36ab4d77bc188fa5c9dcd1dc9709f376eddf6bad7a544eafc55f47a4951263aead9c6bbeff88387a422b95517'
